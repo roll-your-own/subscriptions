376f525e17e0230778e19df0d69edd6f9a5e71bd
Terris Kremer <terris.kremer@gmail.com>
2020-01-29 12:44:31 -0500

    add static route tests

--- src/components/NotAllowed/index.js
+++ src/components/NotAllowed/index.js
@@ -1,2 +1,2 @@
 import React from 'react';
-export const NotAllowed = () => <h4 data-testid="nomatch">You don't have permission to do that.</h4>;
+export const NotAllowed = () => <h4 data-testid="route-notallowed">You don't have permission to do that.</h4>;
