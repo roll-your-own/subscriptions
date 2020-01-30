fffbdbd457a31b3b746cdf8871d85dca123266f3
Terris Kremer <terris.kremer@gmail.com>
2020-01-30 11:02:20 -0500

    add admin controls layout

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { withPermission } from "../Session";
+import { ROUTES } from '../../constants';
 
 const Admin = ({ authUser, dbUser }) => {
   return (
-    <div data-testid="route-admin">
+    <div className="route-admin" data-testid="route-admin">
       <h2>Admin</h2>
+      <div className="admin-controls">
+        <nav className="nav-admin">
+          <Link to={ROUTES.ADMIN}>Admin</Link>
+          <Link to={ROUTES.ADMIN}>Plans</Link>
+          <Link to={ROUTES.ADMIN}>Subscribers</Link>
+          <Link to={ROUTES.ADMIN}>Coupons</Link>
+        </nav>
+        <div className="admin-controls-main">
+          <h3>Admin</h3>
+        </div>
+      </div>
     </div>
   )
 }
