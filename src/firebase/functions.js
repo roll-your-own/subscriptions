b2cfdf208d0bbb6ec77f0a645625c1820a9e5856
Terris Kremer <terris.kremer@gmail.com>
2020-01-30 13:51:58 -0500

    fix plan functions

--- src/firebase/functions.js
+++ src/firebase/functions.js
@@ -2,15 +2,15 @@ import { functions } from './firebase';
 
 export const createPlan = (name, amount, currency, interval) => {
   let createStripePlan = functions.httpsCallable('createStripePlan');
-  return createStripePlan({ uid, name, amount, currency, interval })
+  return createStripePlan({ name, amount, currency, interval })
 }
 
 export const editPlan = (name) => {
   let editStripePlan = functions.httpsCallable('editStripePlan');
-  return editStripePlan({ uid, name })
+  return editStripePlan({ name })
 }
 
 export const deletePlan = (planID, productID) => {
   let deleteStripePlan = functions.httpsCallable('deleteStripePlan');
-  return deleteStripePlan({ uid, planID, productID })
+  return deleteStripePlan({ planID, productID })
 }
