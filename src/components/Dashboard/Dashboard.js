de29d8825b3fc2c840a41fe2159418db8377c4e4
Terris Kremer <terris.kremer@gmail.com>
2020-01-31 11:18:37 -0500

    remove plans from dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,37 +1,14 @@
 import React from "react";
 import { withPermission } from "../Session";
-import { usePlans } from "../Plans/hooks";
 import { Loader, Message } from "../UI";
 import { centsToCurrency } from "../../utils";
 
 const Dashboard = ({ dbUser }) => {
-  const { loading, message, plans } = usePlans();
-
-  if (loading) {
-    return <Loader inline />;
-  }
   return (
     <div className="dashboard container" data-testid="route-dashboard">
       <h2 className="text-center">
         Hey, {dbUser.displayName ? dbUser.displayName : dbUser.email}!
       </h2>
-      {!!message && <Message type={message.type} message={message.message} />}
-      <div className="plan-cards">
-        {!!plans &&
-          plans.map(plan => (
-            <div className="plan-card" key={plan.id}>
-              <h3>{plan.name}</h3>
-              <p>
-                {centsToCurrency(plan.amount)} per {plan.interval}
-              </p>
-              <p>
-                <a href="/dashboard" className="btn">
-                  Subscribe
-                </a>
-              </p>
-            </div>
-          ))}
-      </div>
     </div>
   );
 };
